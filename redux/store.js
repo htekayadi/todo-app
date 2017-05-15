35d27a0e4599b95b62f0c16be782d6b9d997d159
Hendrik Tekayadi <hendrik@popsical.com>
2017-05-15 23:08:55 +0800

    Add Home components

--- redux/store.js
+++ redux/store.js
@@ -1,7 +1,7 @@
-import { createStore, applyMiddleware } from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import reducer from './reducer';
 import thunkMiddleware from 'redux-thunk';
-import { createLogger } from 'redux-logger';
+import {createLogger} from 'redux-logger';
 
 const store = createStore(
   reducer,
